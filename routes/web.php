0644cc1925e6bed78585ab08a39b0518b8468ee0
Ahmed-Te7a <ahmedabdelftah95165@gmail.com>
2017-07-21 18:24:35 +0200

    Modify auth routes

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Route::group(['middleware' => ['guest']],function(){
+
+    Route::get('/login','Auth\LoginController@showLoginForm')->name('login');
+    Route::post('/login','Auth\LoginController@login');
+});
+
+Route::post('logout','Auth\LoginController@logout')->name('logout');
+
 
 Route::get('/home', 'HomeController@index')->name('home');
