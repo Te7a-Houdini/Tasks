5d7b1763525ea0cc2fc2a573e3db8bb83535498b
Ahmed-Te7a <ahmedabdelftah95165@gmail.com>
2017-07-21 19:21:55 +0200

    Modify tasks routes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,7 +28,7 @@
                     </button>
 
                     <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
+                    <a class="navbar-brand" href="{{ route('tasks.index') }}">
                         Tasks
                     </a>
                 </div>
